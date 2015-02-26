105c6412e7c0c6baf1932d7a70ae6b23dba8d985
Bruno Muller <muller.bru@gmail.com>
2015-02-26 22:47:49 +0100

    make.bat

--- make.bat
+++ make.bat
@@ -1,3 +1,5 @@
+cd %1
+D:
 sdasz80 -o crt0.s
 sdcc -mz80 -c main.c
 sdcc -mz80 -c chipset.c
@@ -5,3 +7,4 @@ sdcc -mz80 -c fat16.c
 sdcc -mz80 -c io.c
 sdcc -mz80 --code-loc 0x1509 --data-loc 0x2000 --no-std-crt0 -o bootstrap.ihx main.rel crt0.rel chipset.rel fat16.rel io.rel
 makebin -p bootstrap.ihx bootstrap.bin
+pause
